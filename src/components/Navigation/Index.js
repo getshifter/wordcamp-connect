ba8cf20c4b60d953df40db93a5bf6a381a890489
Daniel Olson <emaildano@gmail.com>
2019-11-01 00:32:22 -0500

    updated nav

--- src/components/Navigation/Index.js
+++ src/components/Navigation/Index.js
@@ -2,9 +2,7 @@ import React, { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
@@ -15,10 +13,6 @@ import {
 } from "reactstrap"
 
 const Navigation = props => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggle = () => setIsOpen(!isOpen)
-
   const data = useStaticQuery(graphql`
     query {
       logoColor: file(relativePath: { eq: "shifter-logo.png" }) {
@@ -40,25 +34,24 @@ const Navigation = props => {
 
   return (
     <div className="navigation">
-    <Container fluid>
-      <Row>
-        <Col>
-          <Navbar dark className="p-3" expand="md">
-            <NavbarBrand href="/">
-              <Img fixed={data.logoWhite.childImageSharp.fixed} alt="Logo" />
-            </NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Navbar dark className="p-3" expand="md">
+              <NavbarBrand href="/">
+                <Img fixed={data.logoWhite.childImageSharp.fixed} alt="Logo" />
+              </NavbarBrand>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <Button color="outline-white" href="https://getshifter.io">getshifter.io</Button>
+                  <Button color="outline-white" href="https://getshifter.io">
+                    getshifter.io
+                  </Button>
                 </NavItem>
               </Nav>
-            </Collapse>
-          </Navbar>
-        </Col>
-      </Row>
-    </Container>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
